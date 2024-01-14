bada596ae20620fca1bd699de4376626b869a71c
AdriaLopezGonzalez <alopezg@edu.tecnocampus.cat>
2024-01-14 19:28:45 +0100

    a

--- GameJam_LosChichos_Jenga/Assets/Scripts/HighScore.cs
+++ GameJam_LosChichos_Jenga/Assets/Scripts/HighScore.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class HighScore : MonoBehaviour
 {
-    public GameController m_GameController;
-    public GameObject value;
+    int value;
+    bool sameFound;
 
-    private void Start()
+    void Start()
     {
-        DontDestroyOnLoad(this);
+        foreach (GameObject highScore in GameObject.FindGameObjectsWithTag("HighScore"))
+        {
+            if (highScore != this.gameObject)
+            {
+                sameFound = true;
+            }
+        }
+
+        if (sameFound)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+        }
     }
 }
